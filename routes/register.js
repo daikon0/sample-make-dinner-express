9bfb3d5b11966a6a98ec7c5f3a7fe96880b2da90
daikon0 <hamada1207.daiki@gmail.com>
2020-03-08 18:40:18 +0900

    エラーが出るよう修正

--- routes/register.js
+++ routes/register.js
@@ -2,12 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const flash = require('express-flash-messages');
+
+router.use(flash());
 
 router.get('/', (req, res, next) => {
   res.render('register')
 });
 
 router.post('/', (req, res, next) => {
+  const passwordCheck = req.body.password.length > 0
+  if (!passwordCheck) {
+    console.log('通ってる');
+    req.flash('error', 'パスワードを入力してください！');
+    return res.redirect('/register');
+  }
   User.create({
     username: req.body.username,
     password: req.body.password
