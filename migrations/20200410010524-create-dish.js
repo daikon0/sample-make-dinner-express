ac6ca8a798556de489704c8014bba8f3699c49e9
daikon0 <hamada1207.daiki@gmail.com>
2020-04-10 13:44:36 +0900

    migration

---
+++ migrations/20200410010524-create-dish.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('dishes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      dishId: {
+        type: Sequelize.UUID
+      },
+      dishName: {
+        type: Sequelize.STRING
+      },
+      dishFile: {
+        type: Sequelize.STRING
+      },
+      dishUrl: {
+        type: Sequelize.STRING
+      },
+      dishGenre: {
+        type: Sequelize.STRING
+      },
+      dishRole: {
+        type: Sequelize.STRING
+      },
+      createdBy: {
+        type: Sequelize.BIGINT
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('dishes');
+  }
+};
