4014e31adac90bed4ca5d288962c12b698bf5f78
daikon0 <hamada1207.daiki@gmail.com>
2020-04-15 14:14:12 +0900

    ログイン時ユーザー名が表示されるよう修正

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe('/login', () => {
 
   it('ログイン時はユーザー名が表示される', (done) => {
     request(app)
-      .get('/login')
+      .get('/')
       .expect(/testuser/)
       .expect(200, done);
   });
