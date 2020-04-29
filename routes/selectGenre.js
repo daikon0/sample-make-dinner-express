dd26d87479588e54abee3cbfbd4744498b37da37
daikon0 <hamada1207.daiki@gmail.com>
2020-04-29 15:37:06 +0900

    Error correction

--- routes/selectGenre.js
+++ routes/selectGenre.js
@@ -13,27 +13,10 @@ router.get("/", authenticationEnsurer, (req, res, next) => {
   });
 });
 
-router.get("/japan", authenticationEnsurer, (req, res, next) => {
-  const maindish = selectDish(req, "和食", "主菜");
-  const subdish = selectDish(req, "和食", "副菜");
-  const soup = selectDish(req, "和食", "汁物");
-  if (maindish === undefined || subdish === undefined || soup === undefined) {
-    req.flash("error", "料理をもっと作ってください！！");
-    res.redirect("/selectGenre");
-  } else {
-    res.render("result", {
-      user: req.user,
-      maindish: maindish,
-      subdish: subdish,
-      soup: soup
-    });
-  }
-});
-
-router.get("/western", authenticationEnsurer, (req, res, next) => {
-  const maindish = selectDish(req, "洋食", "主菜");
-  const subdish = selectDish(req, "洋食", "副菜");
-  const soup = selectDish(req, "洋食", "汁物");
+router.get("/japan", authenticationEnsurer, async (req, res, next) => {
+  const maindish = await selectDish(req, "和食", "主菜");
+  const subdish = await selectDish(req, "和食", "副菜");
+  const soup = await selectDish(req, "和食", "汁物");
 
   if (maindish === undefined || subdish === undefined || soup === undefined) {
     req.flash("error", "料理をもっと作ってください！！");
@@ -48,10 +31,28 @@ router.get("/western", authenticationEnsurer, (req, res, next) => {
   }
 });
 
-router.get("/china", authenticationEnsurer, (req, res, next) => {
-  const maindish = selectDish(req, "中華", "主菜");
-  const subdish = selectDish(req, "中華", "副菜");
-  const soup = selectDish(req, "中華", "汁物");
+router.get("/western", authenticationEnsurer, async (req, res, next) => {
+  const maindish = await selectDish(req, "洋食", "主菜");
+  const subdish = await selectDish(req, "洋食", "副菜");
+  const soup = await selectDish(req, "洋食", "汁物");
+
+  if (maindish === undefined || subdish === undefined || soup === undefined) {
+    req.flash("error", "料理をもっと作ってください！！");
+    res.redirect("/selectGenre");
+  } else {
+    res.render("result", {
+      user: req.user,
+      maindish: maindish,
+      subdish: subdish,
+      soup: soup
+    });
+  }
+});
+
+router.get("/china", authenticationEnsurer, async (req, res, next) => {
+  const maindish = await selectDish(req, "中華", "主菜");
+  const subdish = await selectDish(req, "中華", "副菜");
+  const soup = await selectDish(req, "中華", "汁物");
 
   if (maindish === undefined || subdish === undefined || soup === undefined) {
     req.flash("error", "料理をもっと作ってください！！");
@@ -67,20 +68,17 @@ router.get("/china", authenticationEnsurer, (req, res, next) => {
 });
 
 async function selectDish(req, genre, role) {
-  await db.dish
-    .findAll({
-      where: {
-        dishGenre: genre,
-        createdBy: req.user.id,
-        dishRole: role
-      }
-    })
-    .then(dish => {
-      const dishArray = [];
-      dish.forEach(dish => {
-        dishArray.push(dish);
-      });
-    });
+  const dish = await db.dish.findAll({
+    where: {
+      dishGenre: genre,
+      createdBy: req.user.id,
+      dishRole: role
+    }
+  });
+  const dishArray = [];
+  dish.forEach(dish => {
+    dishArray.push(dish);
+  });
   return romdomDish(dishArray);
 }
 
